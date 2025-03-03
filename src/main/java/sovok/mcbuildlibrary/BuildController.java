4edd86220d3ce2373cf181758a460d0c32ca9fd5
Sovok Totalitarian <mushroomcommunist@gmail.com>
2025-03-03 07:04:59 +0300

    Removed the "controller" package

--- src/main/java/sovok/mcbuildlibrary/BuildController.java
+++ src/main/java/sovok/mcbuildlibrary/BuildController.java
@@ -1,10 +1,9 @@
-package sovok.mcbuildlibrary.controller;
+package sovok.mcbuildlibrary;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sovok.mcbuildlibrary.Build;
 
 @RestController
 public class BuildController {
