c2f70e90a397b6fccb5718a879a1b6a16a8503a4
Sovok Totalitarian <mushroomcommunist@gmail.com>
2025-03-14 10:17:35 +0300

    Fixed SonarCube errors

--- src/main/java/sovok/mcbuildlibrary/exception/GlobalExceptionHandler.java
+++ src/main/java/sovok/mcbuildlibrary/exception/GlobalExceptionHandler.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidQueryParameterException.class)
-    public ResponseEntity<ProblemDetail> handleInvalidQueryParameterException(InvalidQueryParameterException ex) {
+    public ResponseEntity<ProblemDetail>
+        handleInvalidQueryParameterException(InvalidQueryParameterException ex) {
         ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         pd.setDetail(ex.getMessage());
         return ResponseEntity.badRequest().body(pd);
